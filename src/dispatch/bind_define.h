b9bf980cc64b5d65a1dd1cd220987498ebf89739
ernestoCruz05 <a2023144672@isec.pt>
2026-04-10 21:22:57 +0100

    fix: add support for _have_client movement

--- src/dispatch/bind_define.h
+++ src/dispatch/bind_define.h
@@ -1556,16 +1556,13 @@ int32_t viewtoleft_have_client(const Arg *arg) {
 	if (!selmon)
 		return 0;
 
+	if (selmon->isoverview)
+		return 0;
+
 	uint32_t n;
 	uint32_t current = get_tags_first_tag_num(selmon->tagset[selmon->seltags]);
 	bool found = false;
-
-	if (selmon->isoverview) {
-		return 0;
-	}
-
-	if (current <= 1)
-		return 0;
+	bool wrapped = false;
 
 	for (n = current - 1; n >= 1; n--) {
 		if (get_tag_status(n, selmon)) {
@@ -1574,8 +1571,22 @@ int32_t viewtoleft_have_client(const Arg *arg) {
 		}
 	}
 
-	if (found)
+	if (!found && config.tag_carousel) {
+		for (n = LENGTH(tags); n > current; n--) {
+			if (get_tag_status(n, selmon)) {
+				found = true;
+				wrapped = true;
+				break;
+			}
+		}
+	}
+
+	if (found) {
+		if (wrapped)
+			selmon->carousel_anim_dir = -1;
 		view(&(Arg){.ui = (1 << (n - 1)) & TAGMASK, .i = arg->i}, true);
+		selmon->carousel_anim_dir = 0;
+	}
 	return 0;
 }
 
@@ -1583,16 +1594,13 @@ int32_t viewtoright_have_client(const Arg *arg) {
 	if (!selmon)
 		return 0;
 
+	if (selmon->isoverview)
+		return 0;
+
 	uint32_t n;
 	uint32_t current = get_tags_first_tag_num(selmon->tagset[selmon->seltags]);
 	bool found = false;
-
-	if (selmon->isoverview) {
-		return 0;
-	}
-
-	if (current >= LENGTH(tags))
-		return 0;
+	bool wrapped = false;
 
 	for (n = current + 1; n <= LENGTH(tags); n++) {
 		if (get_tag_status(n, selmon)) {
@@ -1601,8 +1609,22 @@ int32_t viewtoright_have_client(const Arg *arg) {
 		}
 	}
 
-	if (found)
+	if (!found && config.tag_carousel) {
+		for (n = 1; n < current; n++) {
+			if (get_tag_status(n, selmon)) {
+				found = true;
+				wrapped = true;
+				break;
+			}
+		}
+	}
+
+	if (found) {
+		if (wrapped)
+			selmon->carousel_anim_dir = 1;
 		view(&(Arg){.ui = (1 << (n - 1)) & TAGMASK, .i = arg->i}, true);
+		selmon->carousel_anim_dir = 0;
+	}
 	return 0;
 }
 
